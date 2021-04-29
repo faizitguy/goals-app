5bfc0904549771ce0c674e756029fd4bd11e0d9c
faizitguy <faizkhan3597@gmail.com>
2021-04-29 06:31:45 +0530

    removed versionKey

--- models/Goal.js
+++ models/Goal.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const goalSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const goalSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = mongoose.model("Goal", goalSchema);

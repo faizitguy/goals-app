ccf94d7a30f9dc584da5c5451245c0e6f24b8a4f
faizitguy <faizkhan3597@gmail.com>
2021-04-29 13:02:28 +0530

    added categories api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const goalRoutes = require("./routes/goals");
+const categoryRoutes = require("./routes/categories");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
@@ -12,7 +13,7 @@ app.use(cors());
 connectDB();
 
 app.use("/", goalRoutes);
-
+app.use("/", categoryRoutes);
 app.get("/", (req, res) => {
   res.status(200).json("Welcome to Goals api");
 });
